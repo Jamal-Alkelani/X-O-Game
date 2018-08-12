382d575e9597f2841064ef213e34259a682ea561
alaamarneh <ala.mamoon@gmail.com>
2018-08-12 19:10:58 +0300

    fdf

--- game.py
+++ game.py
@@ -2,9 +2,19 @@ import gameOver
 import utils
  
 array = [['X',' ',' '],[' ','X',' '],[' ',' ','X']]
+player = 'X'
 def startGame():
 	utils.print_game(array)
-	print (gameOver.gameOver(array,'X'))
+	while True:
+		if gameOver.gameOver(array,player):
+			win()
+			break
+
+def read_input():
+	a = raw_input("input here: ")
+	return a
+def win(player):
+	print()
 
 if __name__ == "__main__":
     startGame()
