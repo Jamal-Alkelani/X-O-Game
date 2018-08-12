389a79a07d9b962e87750e78a0a72fbd22c34136
Jamal-Alkelani <gamalalkelani@gmail.com>
2018-08-12 19:10:07 +0300

    handle_user_input

--- game.py
+++ game.py
@@ -6,5 +6,14 @@ def startGame():
 	utils.print_game(array)
 	print (gameOver.gameOver(array,'X'))
 
+
+
+
+def handle_user_input(position):
+	x=(position - 1) / 3
+	y=(position - 1) % 3
+	return x,y
+
+
 if __name__ == "__main__":
     startGame()
